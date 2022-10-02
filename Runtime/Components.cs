a2f40c85a3fcb3bb959fb5e41bc3084af31440a8
ze0nni <ze0nni@gmail.com>
2022-10-02 14:14:09 +0300

    Init

---
+++ Runtime/Components.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Collections;
+
+namespace Fury
+{
+    [Serializable]
+    public sealed class Components : ISerializable, IEnumerable
+    {
+        readonly Dictionary<Type, object> _map;
+
+        public bool Get<T>(out T component) where T : class
+        {
+            var ok = _map.TryGetValue(typeof(T), out var result);
+            if (!ok)
+            {
+                component = default;
+                return false;
+            }
+            component = result as T;
+            return component != null;
+        }
+
+        public void Set<T>(T component) where T : class
+        {
+            _map[typeof(T)] = component;
+        }
+
+        public void Drop<T>() where T : class
+        {
+            _map.Remove(typeof(T));
+        }
+
+        public Components()
+        {
+            _map = new Dictionary<Type, object>();
+        }
+
+        public Components(SerializationInfo info, StreamingContext context)
+        {
+            _map = new Dictionary<Type, object>();
+
+            var types = info.GetValue("types", typeof(string[])) as string[];
+            foreach (var typeName in types)
+            {
+                var type = Type.GetType(typeName, false);
+                if (type != null)
+                {
+                    _map[type] = info.GetValue(typeName, type);
+                }
+            }
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            var types = _map.Keys.Select(x => x.FullName).ToArray();
+            info.AddValue("types", types);
+            foreach (var (type, value) in _map)
+            {
+                info.AddValue(type.FullName, value);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _map.Values.GetEnumerator();
+        }
+    }
+}
---
+++ Runtime/Identity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Fury
+{
+    [Serializable]
+    public readonly struct Identity<T>
+        : IEquatable<Identity<T>>,
+        IComparable<Identity<T>>,
+        ISerializable
+        where T : class
+    {
+        readonly int _value;
+        public Identity(int value) => _value = value;
+
+        public static Identity<T> Null => new Identity<T>(0);
+        public bool IsNull => _value == 0;
+
+        public static bool operator ==(Identity<T> l, Identity<T> r)
+        {
+            return l._value == r._value;
+        }
+
+        public static bool operator !=(Identity<T> l, Identity<T> r)
+        {
+            return l._value != r._value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Identity<T> other)
+            {
+                return other._value == _value;
+            }
+            return false;
+        }
+
+        public bool Equals(Identity<T> other)
+        {
+            return other._value == _value;
+        }
+
+        public int CompareTo(Identity<T> other)
+        {
+            return _value - other._value;
+        }        
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Identity<{typeof(T).Name}>({_value})";
+        }
+
+        public Identity(SerializationInfo info, StreamingContext context)
+        {
+            _value = info.GetInt32("value");
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("value", _value);
+        }
+    }
+}
